2da8ba33721b51e962d487fc476274be912b3ac1
dramikei <raghav2011rv@gmail.com>
2020-04-25 22:26:17 +0530

    wrote read write clock reset irq functions. rewrote setflags functions

--- src/cpu.rs
+++ src/cpu.rs
@@ -24,7 +24,7 @@ enum Mode {
     NoMode,
 }
 
-enum Interrupt {
+pub enum Interrupt {
     Nmi,
     Reset,
     Irq,
@@ -33,6 +33,7 @@ enum Interrupt {
 
 pub struct CPU {
     pub bus: BUS,
+    cycles: usize,
     pub a:u8,
     pub x:u8,
     pub y:u8,
@@ -43,22 +44,82 @@ pub struct CPU {
     
 }
 
+//TODO: Check if flags work correctly. Places of problems: Reset() set_*FLAG*()
 impl CPU {
     pub fn new(bus: BUS) -> CPU {
         CPU{
             bus,
+            cycles:0,
             a:0,
             x:0,
             y:0,
 
-            p:0b00000100,
+            p:0b00100000,
             sp:0,
             pc:0,
-            
+        }
+    }
+
+    //Read are write functions are here to make CPU struct project-independent.
+    fn read(&mut self, addr: u16) -> u8 {
+        self.bus.read(addr)
+    }
+
+    fn write(&mut self, addr: u16, data: u8) {
+        self.bus.write(addr, data);
+    }
+
+    pub fn clock(&mut self) {
+        if self.cycles == 0 {
+            let opcode = self.read(self.pc);
+            self.pc += 1;
 
         }
     }
 
+    pub fn interrupt(&mut self, interrupt_type: Interrupt) {
+        match interrupt_type {
+            Interrupt::Reset => self.reset(),
+            Interrupt::Irq => self.irq(),
+            _ => panic!("Unimplemented Interrupt called!")
+        }
+    }
+
+    fn reset(&mut self) {
+        let lo: u8 = self.read(0xFFFC);
+        let hi: u8 = self.read(0xFFFC + 1);
+        self.pc = (hi as u16) << 8 | (lo as u16);
+
+        self.a = 0;
+        self.x = 0;
+        self.y = 0;
+        self.sp = 0xFD;
+        self.p = 0b00100100;
+
+        //Reset takes time.
+        self.cycles = 8;
+    }
+
+    fn irq(&mut self) {
+        //TODO: implement Push/POP to stack.
+        if self.get_interrupt_disable() == false {
+            self.write(0x0100 + (self.sp as u16), (self.pc >> 8) as u8);
+            self.sp -= 1;
+            self.write(0x0100 + (self.sp as u16), self.pc as u8);
+            self.sp -= 1;
+
+            self.set_break(false);
+            self.set_unused(true);
+            self.set_interrupt_disable(true);
+            self.write(0x0100 + (self.sp as u16), self.p);
+            self.sp -= 1;
+
+            let lo = self.read(0xFFFE);
+            let hi = self.read(0xFFFE+1);
+            self.pc = ((hi as u16) << 8) | lo as u16;
+            self.cycles = 7;
+        }
+    }
     //Get Flags
     pub fn get_carry(&mut self) -> bool {
         if (self.p & 0b00000001) == 1 { return true } else { return false };
@@ -73,6 +134,9 @@ impl CPU {
     pub fn get_break(&mut self) -> bool {
         if (self.p & 0b00010000) == 1 { return true } else { return false };
     }
+    pub fn get_unsed(&mut self) -> bool {
+        if (self.p & 0b00100000) == 1 { return true } else { return false };
+    }
     pub fn get_overflow(&mut self) -> bool {
         if (self.p & 0b01000000) == 1 { return true } else { return false };
     }
@@ -81,53 +145,60 @@ impl CPU {
     }
 
     //Set Flags
-    pub fn set_carry(&mut self) {
-        self.p |= 0b00000001;
+    pub fn set_carry(&mut self, val: bool) {
+        if val {
+            self.p |= 0b00000001;
+        } else {
+            self.p &= !0b00000001;
+        }
     }
 
-    pub fn set_zero(&mut self) {
-        self.p |= 0b00000010;
+    pub fn set_zero(&mut self, val: bool) {
+        if val {
+            self.p |= 0b00000010;    
+        } else {
+            self.p &= !0b00000010;
+        }
     }
 
-    pub fn set_interrupt_disable(&mut self) {
-        self.p |= 0b00000100;
+    pub fn set_interrupt_disable(&mut self, val: bool) {
+        if val {
+            self.p |= 0b00000100;    
+        } else {
+            self.p &= !0b00000100;
+        }
     }
 
-    pub fn set_break(&mut self) {
-        self.p |= 0b00010000;
+    pub fn set_break(&mut self, val: bool) {
+        if val {
+            self.p |= 0b00010000;    
+        } else {
+            self.p &= !0b00010000;
+        }
     }
 
-    pub fn set_overflow(&mut self) {
-        self.p |= 0b01000000;
+    pub fn set_unused(&mut self, val: bool) {
+        if val {
+            self.p |= 0b00100000;    
+        } else {
+            self.p &= !0b00100000;
+        }
     }
 
-    pub fn set_negative(&mut self) {
-        self.p |= 0b10000000;
+    pub fn set_overflow(&mut self, val: bool) {
+        if val {
+            self.p |= 0b01000000;    
+        } else {
+            self.p &= !0b01000000;
+        }
     }
 
-    //Unset Flags
-    pub fn unset_carry(&mut self) {
-        self.p &= !0b00000001;
-    }
-
-    pub fn unset_zero(&mut self) {
-        self.p &= !0b00000010;
-    }
-
-    pub fn unset_interrupt_disable(&mut self) {
-        self.p &= !0b00000100;
-    }
-
-    pub fn unset_break(&mut self) {
-        self.p &= !0b00010000;
-    }
-
-    pub fn unset_overflow(&mut self) {
-        self.p &= !0b01000000;
-    }
-
-    pub fn unset_negative(&mut self) {
-        self.p &= !0b10000000;
+    pub fn set_negative(&mut self, val: bool) {
+        if val {
+            self.p |= 0b10000000;
+        } else {
+            self.p &= !0b10000000;
+        }
     }
 
 }
